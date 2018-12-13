894592bc820644afcd6f6ebb1f8a42b21a85f6ba
wangtianlun <wangtianlun@souche.com>
2018-12-13 11:37:54 +0800

    feat: 补充示例代码

--- example/src/index.js
+++ example/src/index.js
@@ -1,1 +1,22 @@
-import a from '@src';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Toolbar, Editor } from '@src';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="minder-editor-container" id="minder-container">
+        <Toolbar />
+        <Editor />
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    const minder = new kityminder.Minder({
+      renderTo: '#minder-container'
+    })
+  }
+}
+
+render(<App />, document.getElementById('root'));
