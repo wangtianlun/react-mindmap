66c733904f442c471085088c5c0c623a0547dbe5
wangtianlun <wangtianlun@souche.com>
2019-07-23 17:23:07 +0800

    feat: update demo

--- example/src/index.js
+++ example/src/index.js
@@ -3,26 +3,12 @@ import { render } from 'react-dom';
 import { Toolbar, Editor } from '@src';
 
 export default class App extends Component {
-  state = {
-    minder: null,
-  }
-
   render() {
     return (
       <div className="minder-editor-container">
-        <Editor minder={this.state.minder} />
+        <Editor />
       </div>
     )
-  }
-
-  componentDidMount() {
-    const minder = new kityminder.Minder({
-      renderTo: '#minder-container'
-    });
-      
-    this.setState({
-      minder,
-    });
   }
 }
 
